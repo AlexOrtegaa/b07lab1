f9a46a53bfdc18183d10db4ec69d9c8ed892f150
AlexOrtegaa <89236426+AlexOrtegaa@users.noreply.github.com>
2023-10-04 23:56:19 -0400

    Update Driver.java
    modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -1,5 +1,5 @@
 import java.io.FileNotFoundException;
-import java.io.PrintStream;
+
 import java.io.File;
 
 public class Driver {
@@ -13,8 +13,17 @@ public class Driver {
 		double [] c2 = {1,2, 3, 4};
 		Polynomial p2 = new Polynomial(c2, i1);
 		
+		int [] y = {3,3,3,3};
+		Polynomial rep = new Polynomial(c2, y);
+		Polynomial sumRep = rep.add(p2);
+		
+		
+		
 		Polynomial s = p1.add(p2);
+		
+		Polynomial z = p1.add(p1);
 		s.saveToFile("/Users/alexortega/eclipse-workspace/one/src/output");
+		z.saveToFile("/Users/alexortega/eclipse-workspace/one/src/output2");
 		p1.saveToFile("/Users/alexortega/eclipse-workspace/one/src/input");
 		
 		System.out.println("p2(0.1) = " + p2.evaluate(0.1));
@@ -29,8 +38,14 @@ public class Driver {
 		File file = new File("/Users/alexortega/eclipse-workspace/one/src/input");
 		Polynomial p3 = new Polynomial(file);
 		Polynomial p4 = p1.multiply(p2);
-		p3.saveToFile("/Users/alexortega/eclipse-workspace/one/src/output2");
-		p4.saveToFile("/Users/alexortega/eclipse-workspace/one/src/output3");
+		System.out.println(p4.coefficients.length);
 		
+		
+		p3.saveToFile("/Users/alexortega/eclipse-workspace/one/src/output3");
+		p1.saveToFile("/Users/alexortega/eclipse-workspace/one/src/output5");
+		p2.saveToFile("/Users/alexortega/eclipse-workspace/one/src/output6");
+		
+		p4.saveToFile("/Users/alexortega/eclipse-workspace/one/src/output 4");
+		sumRep.saveToFile("/Users/alexortega/eclipse-workspace/one/src/input");
 	}
 }
